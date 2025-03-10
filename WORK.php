a72a1e7675d8bc878558e688df805b9628994219
Innocent NIYONKURU <169168782+Shimwasergey@users.noreply.github.com>
2025-03-10 16:31:40 +0200

    CYIZA marie claire

---
+++ WORK.php
@@ -0,0 +1,32 @@
+<php>
+    <head>
+        <title>MY CLASS</title>
+        </head>
+        <body>
+            <h1>MY CLASS MATE</h1>
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const classmates = [
+    "Innocent Niyonkuru",
+    "Emmanuel Mugisha",
+    "Jeanpierre Izikiteganya",
+    "Aime Christian Muhire",
+    "Augiste Bwabonimana",
+    "Damianne Mugisha",
+    "Marie Claire Cyiza",
+    "Rachel Ibyimanikora",
+    "Olive Musabyisoni",
+    "Djamillah Mukasangwa"
+];
+
+app.get('/classmates', (req, res) => {
+    res.json({ classmates });
+});
+
+app.listen(PORT, () => {
+   
+});
+</body>
+</php>
